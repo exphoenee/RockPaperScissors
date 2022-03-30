d1fc1d7051ae44b27081f00a9516a452e2cbbc21
exphoenee <bozzay.viktor@gmail.com>
2022-03-30 20:50:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,21 @@ const choice = {
 
 //Get user's choice
 const getUserChoice = (userInput) => {
-  userInput = userInput.toLowerCase();
-  return Object.keys(choice)
-    .map((item) => choice[item] === userInput)
-    .reduce((acc, curr) => (acc += +curr)) > 0
-    ? userInput
-    : console.log("Error! You must select a valid option!");
+  const userChoice =
+    Object.keys(choice)
+      .map((item) => choice[item].name === userInput.name)
+      .reduce((acc, curr) => (acc += +curr)) > 0
+      ? userInput
+      : console.log("Error! You must select a valid option!");
+  console.log("You threw " + userChoice.value);
+  return userChoice;
 };
 
 //Get computer's choice
 const getComputerChoice = () => {
-  return choice[Math.floor(Math.random() * 3)];
+  const computerChoice = choice[Math.floor(Math.random() * 3)];
+  console.log("CPU threw " + computerChoice.value);
+  return computerChoice;
 };
 
 //Compare & determine the winner
@@ -48,14 +52,11 @@ const determineWinner = (userChoice, computerChoice) => {
 };
 
 function getChoice(userChoice) {
-  const uc = userChoice.toLowerCase();
-  console.log(uc);
-  const c = Object.keys(choice).filter((c) => {
-    console.log(choice[c]);
-    return choice[c].name === uc;
-  })[0];
-  console.log(c);
-  return c;
+  return choice[
+    Object.keys(choice).filter(
+      (c) => choice[c].value === userChoice.toLowerCase()
+    )[0]
+  ];
 }
 
 //The program!
@@ -63,8 +64,7 @@ const playGame = () => {
   const userChoice = getUserChoice(getChoice("scissors"));
 
   const computerChoice = getComputerChoice();
-  console.log("You threw " + userChoice);
-  console.log("CPU threw " + computerChoice);
+
   console.log(determineWinner(userChoice, computerChoice));
 };
 
