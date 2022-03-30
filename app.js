4dd7d4ff422248fc594bdc27ce0e55ad8afc1017
exphoenee <bozzay.viktor@gmail.com>
2022-03-31 00:02:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,62 +1,73 @@
-const choice = [
-  { value: "rock", beats: ["scissors", "lizard"] },
-  { value: "paper", beats: ["rock", "spock"] },
-  { value: "scissors", beats: ["paper", "lizard"] },
-  { value: "spock", beats: ["rock", "scissors"] },
-  { value: "lizard", beats: ["paper", "spock"] },
-];
+class Game {
+  constructor() {
+    this.choice = [
+      { value: "rock", beats: ["scissors", "lizard"] },
+      { value: "paper", beats: ["rock", "spock"] },
+      { value: "scissors", beats: ["paper", "lizard"] },
+      { value: "spock", beats: ["rock", "scissors"] },
+      { value: "lizard", beats: ["paper", "spock"] },
+    ];
+    this.generateRules();
+    this.playGame();
+  }
 
-//Get user's choice
-const getUserChoice = (userInput) => {
-  const userChoice =
-    choice
-      .map((item) => item.name === userInput.name)
-      .reduce((acc, curr) => (acc += +curr)) > 0
-      ? userInput
-      : console.log("Error! You must select a valid option!");
-  console.log("You threw " + userChoice.value);
-  return userChoice;
-};
+  //Get user's choice
+  getUserChoice(userInputStr) {
+    const userChoiceObj = this.getChoice(userInputStr);
+    this.userChoice =
+      this.choice
+        .map((item) => item.name === userChoiceObj.name)
+        .reduce((acc, curr) => (acc += +curr)) > 0
+        ? userChoiceObj
+        : console.log("Error! You must select a valid option!");
+  }
 
-//Get computer's choice
-const getComputerChoice = () => {
-  const computerChoice = choice[Math.floor(Math.random() * 5)];
-  console.log("CPU threw " + computerChoice.value);
-  return computerChoice;
-};
+  //Get computer's choice
+  getComputerChoice() {
+    this.computerChoice = this.choice[Math.floor(Math.random() * 5)];
+  }
 
-//Compare & determine the winner
-const determineWinner = (userChoice, computerChoice) => {
-  const result = userChoice.beats.includes(computerChoice.value)
-    ? "You win!"
-    : computerChoice.beats.includes(userChoice.value)
-    ? "Computer wins!"
-    : "This game is a tie!";
-  console.log(result);
-};
+  //Compare & determine the winner
+  determineWinner() {
+    this.result = this.userChoice.beats.includes(this.computerChoice.value)
+      ? "You win!"
+      : this.computerChoice.beats.includes(this.userChoice.value)
+      ? "Computer wins!"
+      : "This game is a tie!";
+  }
 
-function getChoice(userChoice) {
-  return choice[
-    Object.keys(choice).filter(
-      (c) => choice[c].value === userChoice.toLowerCase()
-    )[0]
-  ];
+  getChoice(userChoice) {
+    return this.choice[
+      Object.keys(this.choice).filter(
+        (c) => this.choice[c].value === userChoice.toLowerCase()
+      )[0]
+    ];
+  }
+
+  generateRules() {
+    this.rules = `Game rules:\n${this.choice
+      .map((c) => `${c.value} beats ${c.beats.join(" and ")}`)
+      .join("\n")}\n----------------------------`;
+  }
+
+  showRules() {
+    console.log(this.rules);
+  }
+
+  showResult() {
+    console.log("You threw " + this.userChoice.value);
+    console.log("CPU threw " + this.computerChoice.value);
+    console.log(this.result);
+  }
+
+  //The program!
+  playGame() {
+    this.showRules();
+    this.getUserChoice("scissors");
+    this.getComputerChoice();
+    this.determineWinner();
+    this.showResult();
+  }
 }
 
-function generateRules() {
-  const rules = `Game rules:\n${choice
-    .map((c) => `${c.value} beats ${c.beats.join(" and ")}`)
-    .join("\n")}\n----------------------------`;
-  console.log(rules);
-  return rules;
-}
-
-//The program!
-const playGame = () => {
-  generateRules();
-  const userChoice = getUserChoice(getChoice("scissors"));
-  const computerChoice = getComputerChoice();
-  determineWinner(userChoice, computerChoice);
-};
-
-playGame();
+const game = new Game();
