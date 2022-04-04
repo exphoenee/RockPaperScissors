bf6857fbe45cfd3fc7acb4016d596bb19c24dfdf
exphoenee <bozzay.viktor@gmail.com>
2022-04-04 18:54:03 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,18 @@
 const baseURL = "http://127.0.0.1:5500/media/";
 
-function asynImageLoader(fileName, img) {
+function asynImageLoader(img) {
+  const fileName = img.dataset.filename;
   fetch(`${baseURL}${fileName}`).then((response) =>
     response
       .blob()
-      .then((blob) => (img.src = URL.createObjectURL(blob)))
+      .then((blob) => {
+        img.src = URL.createObjectURL(blob);
+        img.classList.remove("loader-image");
+      })
       .catch((error) => console.log(error))
   );
 }
 
-const imgeName = "scissors.png";
 const img = document.querySelector("img");
 
-asynImageLoader(imgeName, img);
+asynImageLoader(img);
